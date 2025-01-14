51a1cb61f178f18c1cbe96b4c6226f2f505b179f
Kim Dong O <kdo_1999@naver.com>
2025-01-14 17:12:53 +0900

    refactor: ErrorDetail message 필드 제거

--- backend/src/main/java/com/example/backend/global/response/ErrorDetail.java
+++ backend/src/main/java/com/example/backend/global/response/ErrorDetail.java
@@ -1,7 +1,21 @@
 package com.example.backend.global.response;
 
-public record ErrorDetail(String field, String reason, String message) {
-	public static ErrorDetail of(String field, String reason, String message) {
-		return new ErrorDetail(field, reason, message);
+/**
+ * ErrorDetail
+ * <p>@NotNull등과 같은 필드 에러 발생시 처리할 클래스</p>
+ * @param field
+ * @param reason
+ * @author Kim Dong O
+ */
+public record ErrorDetail(String field, String reason) {
+
+	/**
+	 * ErrorDetail 생성 팩토리 메서드
+	 * @param field 예외가 발생한 필드 이름
+	 * @param reason 예외가 발생한 이유
+	 * @return
+	 */
+	public static ErrorDetail of(String field, String reason) {
+		return new ErrorDetail(field, reason);
 	}
 }
