9f5d106a26f784dc4c847b712e00cf894e95f3f0
sobaman <sobaman522@gmail.com>
2025-01-14 16:48:59 +0900

    feat: 주문 컨트롤러 레이어 생성

---
+++ backend/src/main/java/com/example/backend/domain/orders/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.example.backend.domain.orders.controller;
+
+
+import com.example.backend.domain.orders.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+}
