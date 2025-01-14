2d1a08b9295a9a86de57155b163c51b5a1b19cd3
100gammmit <100minha@naver.com>
2025-01-14 17:04:37 +0900

    feat: ProductService 추상체 추가

---
+++ backend/src/main/java/com/example/backend/product/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.example.backend.product.service;
+
+/**
+ * ProductService
+ * 상품 관련 서비스 로직 추상화
+ * @author 100mi
+ */
+public interface ProductService {
+    // 상품 등록
+    void create();
+}
