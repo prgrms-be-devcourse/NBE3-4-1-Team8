8af28955f3523488db9004d299c9bd606b4b1b29
100gammmit <100minha@naver.com>
2025-01-14 16:24:11 +0900

    comment: ProductRepository 주석 추가

--- backend/src/main/java/com/example/backend/product/repository/ProductRepository.java
+++ backend/src/main/java/com/example/backend/product/repository/ProductRepository.java
@@ -4,6 +4,11 @@ import com.example.backend.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * ProductRepository
+ * 상품 관련 Repository
+ * @author 100minha
+ */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
