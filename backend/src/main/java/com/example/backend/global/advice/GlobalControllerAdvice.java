03f9ae2933797639f85c1e80a291fd170d7d68df
Kim Dong O <kdo_1999@naver.com>
2025-01-14 20:56:23 +0900

    feat: GlobalControllerAdvice 추가

---
+++ backend/src/main/java/com/example/backend/global/advice/GlobalControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.example.backend.global.advice;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.example.backend.global.response.ErrorDetail;
+import com.example.backend.global.response.HttpErrorInfo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * GlobalControllerAdvice
+ * <p>애플리케이션 전역에서 발생하는 예외를 처리하는 클래스 입니다.</p>
+ * @author Kim Dong O
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalControllerAdvice {
+
+
+}
