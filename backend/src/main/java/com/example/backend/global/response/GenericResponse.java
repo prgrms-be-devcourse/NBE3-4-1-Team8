1ed611f4dc2cdbadccebeca090f64e6d5c86d199
Kim Dong O <kdo_1999@naver.com>
2025-01-14 15:41:59 +0900

    feat: Builder 추가

--- backend/src/main/java/com/example/backend/global/response/GenericResponse.java
+++ backend/src/main/java/com/example/backend/global/response/GenericResponse.java
@@ -2,6 +2,8 @@ package com.example.backend.global.response;
 
 import java.time.ZonedDateTime;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 
 /**
@@ -17,6 +19,7 @@ public class GenericResponse<T> {
 	private String message;
 	private final T data;
 
+	@Builder(access = AccessLevel.PRIVATE)
 	private GenericResponse(T data, String message, boolean isSuccess) {
 		this.timestamp = ZonedDateTime.now();
 		this.data = data;
