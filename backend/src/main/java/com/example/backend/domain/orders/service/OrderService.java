3927c49b804bfaa45b64144b30c27154e762f7d0
sobaman <sobaman522@gmail.com>
2025-01-14 16:43:32 +0900

    feat: 주문 서비스 레이어 생성

---
+++ backend/src/main/java/com/example/backend/domain/orders/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.example.backend.domain.orders.service;
+
+
+import com.example.backend.domain.orders.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+}
