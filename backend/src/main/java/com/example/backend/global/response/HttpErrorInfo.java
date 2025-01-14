4edab3330176d98b5155827a710b22598510b3da
Kim Dong O <kdo_1999@naver.com>
2025-01-14 17:16:44 +0900

    comment: 주석 추가

--- backend/src/main/java/com/example/backend/global/response/HttpErrorInfo.java
+++ backend/src/main/java/com/example/backend/global/response/HttpErrorInfo.java
@@ -3,8 +3,27 @@ package com.example.backend.global.response;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+/**
+ * HttpErrorInfo
+ * <p>예외 발생시 사용할 공통 응답 클래스</p>
+ * @param code
+ * @param path
+ * @param message
+ * @param errorDetails
+ * @param timeStamp
+ * @author Kim Dong O
+ */
 public record HttpErrorInfo(String code, String path, String message, List<ErrorDetail> errorDetails, ZonedDateTime timeStamp) {
-	// of 메서드를 통한 팩토리 메서드 구현
+	/**
+	 * HttpErrorInfo 생성 팩토리 메서드
+	 *
+	 * @param code 커스텀 에외 코드
+	 * @param path 예외가 발생한 요청 경로
+	 * @param message 예외가 발생한 이유
+	 * @param errorDetails 필드 에러 정보
+	 * @author Kim Dong O
+	 * @return {@link HttpErrorInfo}
+	 */
 	public static HttpErrorInfo of(String code, String path, String message, List<ErrorDetail> errorDetails) {
 		return new HttpErrorInfo(code, path, message, errorDetails, ZonedDateTime.now());
 	}
