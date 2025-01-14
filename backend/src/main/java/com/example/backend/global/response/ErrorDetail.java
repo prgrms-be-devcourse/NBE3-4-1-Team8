4275d9bc501b4e60d2a4592b17adccea152058b5
Kim Dong O <kdo_1999@naver.com>
2025-01-14 17:14:09 +0900

    comment: 주석 수정

--- backend/src/main/java/com/example/backend/global/response/ErrorDetail.java
+++ backend/src/main/java/com/example/backend/global/response/ErrorDetail.java
@@ -13,7 +13,7 @@ public record ErrorDetail(String field, String reason) {
 	 * ErrorDetail 생성 팩토리 메서드
 	 * @param field 예외가 발생한 필드 이름
 	 * @param reason 예외가 발생한 이유
-	 * @return
+	 * @return {@link ErrorDetail}
 	 */
 	public static ErrorDetail of(String field, String reason) {
 		return new ErrorDetail(field, reason);
