4b33985c8c659c856068bd78d7ae7e8a2b93a247
Kim Dong O <kdo_1999@naver.com>
2025-01-14 16:42:30 +0900

    comment: 주석 제거

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,7 +3,6 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//디스코드 알림 테스트 커밋
 @SpringBootApplication
 public class BackendApplication {
 
