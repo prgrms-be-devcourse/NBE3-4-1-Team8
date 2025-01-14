8b75dc511b68b47ed620b4cdc40ef03fc0b0ebc6
Kim Dong O <kdo_1999@naver.com>
2025-01-14 16:40:47 +0900

    comment: 디스코드 알림 테스트 커밋

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//디스코드 알림 테스트 커밋
 @SpringBootApplication
 public class BackendApplication {
 
