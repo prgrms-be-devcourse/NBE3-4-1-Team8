84c2f4489c033b7457c397e73a27482cb4d2b4e3
100gammmit <100minha@naver.com>
2025-01-15 14:34:33 +0900

    feat : ProductResponse로 반환하는 조회 쿼리 추가
    컨트롤러에 바로 DTO로 반환하기 위해 추가

--- backend/src/main/java/com/example/backend/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/com/example/backend/domain/product/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.example.backend.domain.product.repository;
 
+import com.example.backend.domain.product.dto.ProductResponse;
 import com.example.backend.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * ProductRepository
@@ -11,4 +16,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT new com.example.backend.domain.product.dto.ProductResponse(p.id, p.name, p.content, " +
+            "p.price, p.imgUrl, p.quantity) " +
+            "FROM Product p " +
+            "WHERE p.id = :id")
+    Optional<ProductResponse> findProductResponseById(@Param("id") Long id);
 }
