69371c4e586db8d1af4ce99f358c479124c5e235
vdvhk12 <vdvhk12@gmail.com>
2025-01-15 17:18:02 +0900

    feat: 로그인 기능 구현 #9
    - AuthController
      - AuthForm으로 username, password 받음
      - 리프레시 토큰은 Set-Cookie로 넘겨줌
      - 쿠키 유효 기간은 REFRESH_TOKEN_EXPIRATION_TIME을 JwtProvider에서 가져와서 사용
      - 액세스 토큰은 헤더의 "Authorization" 으로 "Bearer " 붙여서 넘김
    - AuthService
      - username으로 멤버 조회후 없으면 USER_NOT_FOUND
      - 비밀번호 불일치 시 PASSWORD_NOT_MATCH
      - 액세스 토큰, 리프레시 토큰 각각 생성 후 리턴
      - 리프레시 토큰 생성후 멤버 엔티티에 저장 로직 포함

---
+++ backend/src/main/java/com/example/backend/global/auth/dto/AuthForm.java
@@ -0,0 +1,18 @@
+package com.example.backend.global.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthForm {
+
+    @Email
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    private String password;
+}
