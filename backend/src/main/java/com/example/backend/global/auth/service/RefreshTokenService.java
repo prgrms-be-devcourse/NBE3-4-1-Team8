e302555aad3198c7764fd1d5aad22773c9d45235
vdvhk12 <vdvhk12@gmail.com>
2025-01-15 19:59:45 +0900

    feat: redisConfig 설정 추가 & RefreshTokenService 추가
    - saveRefreshToken: 리프레시 토큰 저장, 레디스에서 유효기간 7일, JwtProvider에서도 리프레시 토큰 유효기간 7일로 수정
    - getRefreshToken: 리프레시 토큰 조회
    - deleteRefreshToken: 리프레시 토큰 삭제

---
+++ backend/src/main/java/com/example/backend/global/auth/service/RefreshTokenService.java
@@ -0,0 +1,25 @@
+package com.example.backend.global.auth.service;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void  saveRefreshToken(String username, String refreshToken) {
+        redisTemplate.opsForValue().set(username, refreshToken, 7, TimeUnit.DAYS);
+    }
+
+    public String getRefreshToken(String username) {
+        return redisTemplate.opsForValue().get(username);
+    }
+
+    public void deleteRefreshToken(String username) {
+        redisTemplate.delete(username);
+    }
+}
