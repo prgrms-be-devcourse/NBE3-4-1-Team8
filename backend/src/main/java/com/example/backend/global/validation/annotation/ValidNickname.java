53176ee92c4cab25a0042df941e3704e30400d91
Kim Dong O <kdo_1999@naver.com>
2025-01-15 16:47:08 +0900

    feat: 회원 Nickname 검증기 추가

---
+++ backend/src/main/java/com/example/backend/global/validation/annotation/ValidNickname.java
@@ -0,0 +1,29 @@
+package com.example.backend.global.validation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.example.backend.global.validation.validator.ValidNicknameValidator;
+import com.example.backend.global.validation.validator.ValidUsernameValidator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+/**
+ * ValidNickname
+ * <p>검증할 Nickname 필드에 사용할 어노테이션</p>
+ * <p>Validation: {@link ValidNicknameValidator}</p>
+ * @author Kim Dong O
+ */
+@Constraint(validatedBy = ValidNicknameValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidNickname {
+	String message() default "유효하지 않은 회원 이름 입니다.";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
