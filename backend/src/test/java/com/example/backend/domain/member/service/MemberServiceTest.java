e11a484f88544d92b80f4e75ecb31ff5f5986e66
Kim Dong O <kdo_1999@naver.com>
2025-01-15 22:04:33 +0900

    test: MemberServiceTest.signup_exists_username_fail 추가

--- backend/src/test/java/com/example/backend/domain/member/service/MemberServiceTest.java
+++ backend/src/test/java/com/example/backend/domain/member/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.example.backend.domain.member.service;
 
 import static org.mockito.BDDMockito.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,8 @@ import com.example.backend.domain.member.dto.MemberDto;
 import com.example.backend.domain.member.dto.MemberSignupRequest;
 import com.example.backend.domain.member.entity.Member;
 import com.example.backend.domain.member.entity.Role;
+import com.example.backend.domain.member.exception.MemberErrorCode;
+import com.example.backend.domain.member.exception.MemberException;
 import com.example.backend.domain.member.repository.MemberRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -80,4 +83,47 @@ class MemberServiceTest {
 		verify(memberRepository, times(1)).save(any(Member.class));
 	}
 
+	@DisplayName("회원가입 이메일 중복 실패 테스트")
+	@Test
+	void signup_exists_username_fail() {
+		MemberSignupRequest givenMemberSignupRequest = MemberSignupRequest.builder()
+			.username("testEmail@naver.com")
+			.nickname("testNickName")
+			.password("!testPassword1234")
+			.passwordCheck("!testPassword1234")
+			.city("testCity")
+			.detail("testDetail")
+			.country("testCountry")
+			.district("testDistrict")
+			.verifyCode("testCode")
+			.build();
+
+		Address givenAddress = Address.builder()
+			.city(givenMemberSignupRequest.city())
+			.detail(givenMemberSignupRequest.detail())
+			.country(givenMemberSignupRequest.country())
+			.district(givenMemberSignupRequest.district())
+			.build();
+
+		MemberDto givenMember = MemberDto.builder()
+			.username(givenMemberSignupRequest.username())
+			.nickname(givenMemberSignupRequest.nickname())
+			.password(givenMemberSignupRequest.password())
+			.address(givenAddress)
+			.role(Role.ROLE_USER)
+			.build();
+
+		Member givenMemberEntity = Member.from(givenMember);
+
+		given(memberRepository.existsByUsername(givenMember.username())).willReturn(true);
+		given(memberRepository.existsByNickname(givenMember.nickname())).willReturn(false);
+
+		//when & then
+		Assertions.assertThatThrownBy(() -> memberService.signup(givenMemberSignupRequest.username(), givenMemberSignupRequest.nickname(),
+			givenMemberSignupRequest.password(), givenMemberSignupRequest.passwordCheck(),
+			givenMemberSignupRequest.city(), givenMemberSignupRequest.district(), givenMemberSignupRequest.country(),
+			givenMemberSignupRequest.detail()))
+			.isInstanceOf(MemberException.class)
+			.hasMessage(MemberErrorCode.EXISTS_USERNAME.getMessage());
+	}
 }
