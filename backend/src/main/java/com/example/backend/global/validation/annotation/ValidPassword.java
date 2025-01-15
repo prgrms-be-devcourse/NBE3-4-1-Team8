178e1176333565f77f3d676ea56e1ccb37c2f342
Kim Dong O <kdo_1999@naver.com>
2025-01-15 16:46:53 +0900

    feat: 회원 Password 검증기 추가

---
+++ backend/src/main/java/com/example/backend/global/validation/annotation/ValidPassword.java
@@ -0,0 +1,28 @@
+package com.example.backend.global.validation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.example.backend.global.validation.validator.ValidPasswordValidator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+/**
+ * ValidPassword
+ * <p>검증할 Password 필드에 사용할 어노테이션</p>
+ * <p>Validation: {@link ValidPasswordValidator}</p>
+ * @author Kim Dong O
+ */
+@Constraint(validatedBy = ValidPasswordValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+	String message() default "공백 없이 비밀번호는 최소 8자리, 최대 20자리이며 대소문자, 숫자, 특수문자 1개씩 필수 입력해야 합니다.";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
