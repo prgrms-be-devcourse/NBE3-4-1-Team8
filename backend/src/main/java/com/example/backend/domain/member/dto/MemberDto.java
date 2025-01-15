bb1486e6c94de58d3e19c28de6230dc7d5058b6a
Kim Dong O <kdo_1999@naver.com>
2025-01-15 12:24:10 +0900

    feat: member 추가

---
+++ backend/src/main/java/com/example/backend/domain/member/dto/MemberDto.java
@@ -0,0 +1,26 @@
+package com.example.backend.domain.member.dto;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+import com.example.backend.domain.common.Address;
+import com.example.backend.domain.member.entity.Role;
+
+import lombok.Builder;
+
+public record MemberDto(Long id, String username, String nickname, String password, Role role, Address address, LocalDateTime createdAt,
+		LocalDateTime modifiedAt) {
+
+	@Builder
+	public MemberDto(Long id, String username, String nickname, String password, Role role, Address address, LocalDateTime createdAt,
+		LocalDateTime modifiedAt) {
+		this.id = id;
+		this.username = username;
+		this.nickname = nickname;
+		this.password = password;
+		this.role = role;
+		this.address = address;
+		this.createdAt = createdAt;
+		this.modifiedAt = modifiedAt;
+	}
+}
