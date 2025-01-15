36c7dbd616123274be565378af23f42a2e9c5a53
Kim Dong O <kdo_1999@naver.com>
2025-01-15 23:27:19 +0900

    feat: MemberController 추가

---
+++ backend/src/main/java/com/example/backend/domain/member/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.example.backend.domain.member.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.domain.member.dto.MemberSignupRequest;
+import com.example.backend.domain.member.service.MemberService;
+import com.example.backend.global.response.GenericResponse;
+import com.example.backend.global.validation.ValidationSequence;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class MemberController {
+	private final MemberService memberService;
+
+	@PostMapping("/join")
+	public ResponseEntity<GenericResponse<Void>> signUp(
+		@RequestBody @Validated(ValidationSequence.class) MemberSignupRequest memberSignupRequest) {
+
+		memberService.signup(memberSignupRequest.username(), memberSignupRequest.nickname(),
+			memberSignupRequest.password(), memberSignupRequest.verifyCode(), memberSignupRequest.city(),
+			memberSignupRequest.district(), memberSignupRequest.country(), memberSignupRequest.detail());
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(GenericResponse.of());
+	}
+}
