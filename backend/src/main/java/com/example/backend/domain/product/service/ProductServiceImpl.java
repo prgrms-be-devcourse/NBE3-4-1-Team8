0e288cfdc04b94ea20a253bae03cecc8706303a0
100gammmit <100minha@naver.com>
2025-01-15 12:47:08 +0900

    feat : Product 조회 시 서비스 레이어에서 Response DTO로 반환하는 메서드 추가
    컨트롤러에 바로 DTO로 반환하기 위해 추가

--- backend/src/main/java/com/example/backend/domain/product/service/ProductServiceImpl.java
+++ backend/src/main/java/com/example/backend/domain/product/service/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.backend.domain.product.service;
 
 import com.example.backend.domain.product.dto.ProductForm;
+import com.example.backend.domain.product.dto.ProductResponse;
 import com.example.backend.domain.product.entity.Product;
+import com.example.backend.domain.product.exception.ProductErrorCode;
+import com.example.backend.domain.product.exception.ProductException;
 import com.example.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * ProductServiceImpl
@@ -16,6 +20,21 @@ import org.springframework.stereotype.Service;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Product findById(Long id) {
+
+        return productRepository.findById(id).orElseThrow(()
+                -> new ProductException(ProductErrorCode.NOT_FOUND));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ProductResponse findProductResponseById(Long id) {
+
+        return ProductResponse.of(this.findById(id));
+    }
 
     @Override
     public void create(ProductForm productForm) {
@@ -30,5 +49,7 @@ public class ProductServiceImpl implements ProductService {
 
         productRepository.save(product);
     }
+
+
 
 }
