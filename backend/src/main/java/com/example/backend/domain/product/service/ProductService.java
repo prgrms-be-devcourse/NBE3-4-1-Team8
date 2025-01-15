8e809d06349c171d70d8a6a11243827e13c2bbcf
100gammmit <100minha@naver.com>
2025-01-15 14:46:11 +0900

    feat : ProductService 추상체 -> 구현체 하나만 사용 하는 것으로 변경

--- backend/src/main/java/com/example/backend/domain/product/service/ProductService.java
+++ backend/src/main/java/com/example/backend/domain/product/service/ProductService.java
@@ -3,19 +3,51 @@ package com.example.backend.domain.product.service;
 import com.example.backend.domain.product.dto.ProductForm;
 import com.example.backend.domain.product.dto.ProductResponse;
 import com.example.backend.domain.product.entity.Product;
+import com.example.backend.domain.product.exception.ProductErrorCode;
+import com.example.backend.domain.product.exception.ProductException;
+import com.example.backend.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- * ProductService
- * 상품 관련 서비스 로직 추상화
- * @author 100mi
+ * ProductServiceImpl
+ * 상품 관련 서비스 로직 구현
+ * @author 100minha
  */
-public interface ProductService {
-    // 상품 단건 조회 (Entity)
-    Product findById(Long id);
-    // 상품 단건 조회 (DTO)
-    public ProductResponse findProductResponseById(Long id);
-    // 상품 등록
-    void create(ProductForm productForm);
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public Product findById(Long id) {
+
+        return productRepository.findById(id).orElseThrow(()
+                -> new ProductException(ProductErrorCode.NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public ProductResponse findProductResponseById(Long id) {
+
+        return productRepository.findProductResponseById(id).orElseThrow(()
+                -> new ProductException(ProductErrorCode.NOT_FOUND));
+    }
+
+    public void create(ProductForm productForm) {
+
+        Product product = Product.builder()
+                .name(productForm.name())
+                .content(productForm.content())
+                .price(productForm.price())
+                .imgUrl(productForm.imgUrl())
+                .quantity(productForm.quantity())
+                .build();
+
+        productRepository.save(product);
+    }
+
 
 
 }
