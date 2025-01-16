02bc585f293b2a8779d6342e66b417e8c48bf02c
Kim Dong O <kdo_1999@naver.com>
2025-01-16 12:16:31 +0900

    feat: MemberStatus 추가

--- backend/src/main/java/com/example/backend/domain/member/dto/MemberDto.java
+++ backend/src/main/java/com/example/backend/domain/member/dto/MemberDto.java
@@ -1,23 +1,24 @@
 package com.example.backend.domain.member.dto;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 import com.example.backend.domain.common.Address;
+import com.example.backend.domain.member.entity.MemberStatus;
 import com.example.backend.domain.member.entity.Role;
 
 import lombok.Builder;
 
-public record MemberDto(Long id, String username, String nickname, String password, Role role, Address address, ZonedDateTime createdAt,
-		ZonedDateTime modifiedAt) {
+public record MemberDto(Long id, String username, String nickname, String password, MemberStatus memberStatus, Role role, Address address, ZonedDateTime createdAt,
+						ZonedDateTime modifiedAt) {
 
 	@Builder
-	public MemberDto(Long id, String username, String nickname, String password, Role role, Address address, ZonedDateTime createdAt,
+	public MemberDto(Long id, String username, String nickname, String password, MemberStatus memberStatus, Role role, Address address, ZonedDateTime createdAt,
 		ZonedDateTime modifiedAt) {
 		this.id = id;
 		this.username = username;
 		this.nickname = nickname;
 		this.password = password;
+		this.memberStatus = memberStatus;
 		this.role = role;
 		this.address = address;
 		this.createdAt = createdAt;
