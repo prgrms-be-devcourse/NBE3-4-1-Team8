2faef885e82c3df5fabec75e8c3654d0805686d7
vdvhk12 <vdvhk12@gmail.com>
2025-01-16 11:13:58 +0900

    refactor: RedisTemplate 빈 충돌 문제 해결
    - RefreshTokenService에서 추가한 의존성에서 RedisConfig 에서 등록한 빈이랑 redis 설정시 기본적으로 등록되는 빈이랑 이름이 같아서 빈 충돌 발생
    - RedisConfig에 작성한 빈을 삭제하고, 기본 생성되는 빈을 사용하도록 수정
    - @Primany 어노테이션을 붙여도 해결가능한데, redis에서 제공하는 템플릿을 사용

--- backend/src/main/java/com/example/backend/global/auth/service/RefreshTokenService.java
+++ backend/src/main/java/com/example/backend/global/auth/service/RefreshTokenService.java
@@ -2,14 +2,14 @@ package com.example.backend.global.auth.service;
 
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final StringRedisTemplate redisTemplate;
 
     public void  saveRefreshToken(String username, String refreshToken) {
         redisTemplate.opsForValue().set(username, refreshToken, 7, TimeUnit.DAYS);
