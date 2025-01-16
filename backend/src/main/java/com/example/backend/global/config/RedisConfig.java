e3d6418e23326c99a0594e5e4b66845da2adf2cc
Kim Dong O <kdo_1999@naver.com>
2025-01-16 15:02:19 +0900

    refactor: rebase 충돌 부분 수정 #16

--- backend/src/main/java/com/example/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/example/backend/global/config/RedisConfig.java
@@ -1,8 +1,25 @@
 package com.example.backend.global.config;
 
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
+        redisTemplate.setValueSerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
+		redisTemplate.setHashValueSerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
+        redisTemplate.setEnableTransactionSupport(true);
+        return redisTemplate;
+    }
 }
