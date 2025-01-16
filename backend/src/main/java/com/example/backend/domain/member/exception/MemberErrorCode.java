26db8b8b4361faf6a67c94fc3ce68ac16c9e8b05
sobaman <sobaman522@gmail.com>
2025-01-16 15:17:59 +0900

    feat: 회원 에러 코드 추가

--- backend/src/main/java/com/example/backend/domain/member/exception/MemberErrorCode.java
+++ backend/src/main/java/com/example/backend/domain/member/exception/MemberErrorCode.java
@@ -13,7 +13,12 @@ import lombok.Getter;
 @Getter
 public enum MemberErrorCode {
 	EXISTS_USERNAME(HttpStatus.BAD_REQUEST, "400-1", "중복된 이메일 입니다."),
-	EXISTS_NICKNAME(HttpStatus.BAD_REQUEST, "400-2", "중복된 닉네임 입니다.");
+	EXISTS_NICKNAME(HttpStatus.BAD_REQUEST, "400-2", "중복된 닉네임 입니다."),
+	FORBIDDEN(HttpStatus.FORBIDDEN, "403-1", "접근 권한이 없습니다."),
+	NOT_FOUND(HttpStatus.NOT_FOUND,"404-1", "해당 리소스를 찾을 수 없습니다"),
+	SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500-1", "서버 에러"),
+	NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR, "500-2", "Null Point");
+
 
 	final HttpStatus httpStatus;
 	final String code;
