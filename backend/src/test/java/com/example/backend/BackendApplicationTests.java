595ebba6f4b59b9d0af718161cf3a09384dcd566
Kim Dong O <kdo_1999@naver.com>
2025-01-18 00:27:14 +0900

    test: import 추가 #36

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,7 +2,14 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+import com.example.backend.global.config.CorsConfig;
+import com.example.backend.global.config.MailConfig;
+import com.example.backend.global.config.RedisConfig;
+import com.example.backend.global.config.TestSecurityConfig;
+
+@Import({MailConfig.class, CorsConfig.class, TestSecurityConfig.class, RedisConfig.class})
 @SpringBootTest
 class BackendApplicationTests {
 
