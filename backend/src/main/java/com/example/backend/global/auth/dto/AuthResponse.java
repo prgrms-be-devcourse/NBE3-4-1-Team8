6179dbe2678f6f3b2add617528914fa78acfcf88
vdvhk12 <vdvhk12@gmail.com>
2025-01-17 18:24:07 +0900

    refactor: AuthResponse 필트에 id 삭제

--- backend/src/main/java/com/example/backend/global/auth/dto/AuthResponse.java
+++ backend/src/main/java/com/example/backend/global/auth/dto/AuthResponse.java
@@ -7,14 +7,12 @@ import lombok.Getter;
 @Builder
 public class AuthResponse {
 
-    private Long id;
     private String username;
     private String accessToken;
     private String refreshToken;
 
-    public static AuthResponse of(Long id, String username, String accessToken, String refreshToken) {
+    public static AuthResponse of(String username, String accessToken, String refreshToken) {
         return AuthResponse.builder()
-            .id(id)
             .username(username)
             .accessToken(accessToken)
             .refreshToken(refreshToken)
