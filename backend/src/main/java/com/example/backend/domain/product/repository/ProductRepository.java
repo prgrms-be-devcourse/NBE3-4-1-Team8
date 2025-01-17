e60694f4cc5743671c5ec6883c59147e15d89014
100gammmit <100minha@naver.com>
2025-01-17 15:03:42 +0900

    feat: 다건 조회 구현 및 필요 메서드 구현
    - 반환 타입 : Page<ProductResponse>
    
    issue : #29

--- backend/src/main/java/com/example/backend/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/com/example/backend/domain/product/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import com.example.backend.domain.product.dto.ProductResponse;
 import com.example.backend.domain.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -27,5 +28,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT new com.example.backend.domain.product.dto.ProductResponse(p.id, p.name, p.content, p.price, p.imgUrl) " +
             "FROM Product p")
-    Page<ProductResponse> findAllPaged(PageRequest pageRequest);
+    Page<ProductResponse> findAllPaged(Pageable pageable);
 }
