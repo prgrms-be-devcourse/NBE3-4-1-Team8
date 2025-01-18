f24ee2342c76ccedd8a1d076f8a65488331e09bd
wjwj910 <wjwj910@gmail.com>
2025-01-18 20:41:03 +0900

    feat: 재고 부족 예외코드 추가

--- backend/src/main/java/com/example/backend/domain/cart/exception/CartErrorCode.java
+++ backend/src/main/java/com/example/backend/domain/cart/exception/CartErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CartErrorCode {
     ALREADY_EXISTS_IN_CART(HttpStatus.BAD_REQUEST, "이미 장바구니에 추가된 상품입니다.", "400-1"),
-    INVALID_QUANTITY(HttpStatus.BAD_REQUEST,"상품을 최소 1개 이상 추가하여야 합니다." ,"400-2");
+    INVALID_QUANTITY(HttpStatus.BAD_REQUEST,"상품을 최소 1개 이상 추가하여야 합니다." ,"400-2"),
+    INSUFFICIENT_STOCK(HttpStatus.BAD_REQUEST,"재고가 부족합니다." ,"400-3");
 
     final HttpStatus httpStatus;
     final String message;
