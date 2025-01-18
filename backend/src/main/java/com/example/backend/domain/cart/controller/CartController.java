d8c14b68afb6f833190ce7f0cab25fd061923515
wjwj910 <wjwj910@gmail.com>
2025-01-19 04:15:39 +0900

    feat: 장바구니 조회 메서드(getCarts) 추가

--- backend/src/main/java/com/example/backend/domain/cart/controller/CartController.java
+++ backend/src/main/java/com/example/backend/domain/cart/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.example.backend.domain.cart.controller;
 
 import com.example.backend.domain.cart.dto.CartForm;
+import com.example.backend.domain.cart.dto.CartResponse;
 import com.example.backend.domain.cart.service.CartService;
 import com.example.backend.domain.member.entity.Member;
 import com.example.backend.global.auth.model.CustomUserDetails;
@@ -10,10 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +31,17 @@ public class CartController {
         Long cartId = cartService.addCartItem(cartForm, member);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(GenericResponse.of(cartId));
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<GenericResponse<List<CartResponse>>> getCarts(
+            @PathVariable("id") Long memberId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        Member member = customUserDetails.getMember();
+
+        List<CartResponse> cartResponses = cartService.getCartsByMember(memberId, member);
+
+        return ResponseEntity.ok(GenericResponse.of(cartResponses));
     }
 }
