4574cafa918012ae2b0379447423e901dca1f33a
100gammmit <100minha@naver.com>
2025-01-18 12:42:51 +0900

    feat: 엔티티와 dto간의 변환 메서드를 관리하는 Converter클래스 추가
    issue : #29

---
+++ backend/src/main/java/com/example/backend/domain/product/converter/ProductConverter.java
@@ -0,0 +1,39 @@
+package com.example.backend.domain.product.converter;
+
+import com.example.backend.domain.product.dto.ProductForm;
+import com.example.backend.domain.product.dto.ProductResponse;
+import com.example.backend.domain.product.entity.Product;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * ProductConverter
+ * 엔티티, DTO간의 변환 메서드를 관리하는 클래스
+ *
+ * @author 100minha
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductConverter {
+
+    public static Product from(ProductForm productForm) {
+
+        return Product.builder()
+                .name(productForm.name())
+                .content(productForm.content())
+                .price(productForm.price())
+                .imgUrl(productForm.imgUrl())
+                .quantity(productForm.quantity())
+                .build();
+    }
+
+    public static ProductResponse from(Product product) {
+
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getContent(),
+                product.getPrice(),
+                product.getImgUrl()
+        );
+    }
+}
