34c007f118b5689e01d0d0db528a72b0f0c68fb9
vdvhk12 <vdvhk12@gmail.com>
2025-01-18 18:25:59 +0900

    refactor: 멤버 조회 응답 데이터에 memberStatus 삭제

--- backend/src/main/java/com/example/backend/domain/member/dto/MemberInfoResponse.java
+++ backend/src/main/java/com/example/backend/domain/member/dto/MemberInfoResponse.java
@@ -2,18 +2,16 @@ package com.example.backend.domain.member.dto;
 
 import com.example.backend.domain.common.Address;
 import com.example.backend.domain.member.entity.Member;
-import com.example.backend.domain.member.entity.MemberStatus;
 
 import lombok.Builder;
 
 @Builder
-public record MemberInfoResponse (String username, String nickname, MemberStatus memberStatus, Address address){
+public record MemberInfoResponse (String username, String nickname, Address address){
 
 	public static MemberInfoResponse of(Member member){
 		return MemberInfoResponse.builder()
 			.username(member.getUsername())
 			.nickname(member.getNickname())
-			.memberStatus(member.getMemberStatus())
 			.address(member.getAddress())
 			.build();
 	}
