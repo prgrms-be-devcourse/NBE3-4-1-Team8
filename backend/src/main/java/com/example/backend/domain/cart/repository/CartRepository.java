1bcec98533d5123eaf62dfd1af44c7fa9c9d27b6
wjwj910 <wjwj910@gmail.com>
2025-01-19 04:19:28 +0900

    feat: 회원별 장바구니를 조회하는 쿼리 메서드 추가

--- backend/src/main/java/com/example/backend/domain/cart/repository/CartRepository.java
+++ backend/src/main/java/com/example/backend/domain/cart/repository/CartRepository.java
@@ -1,9 +1,17 @@
 package com.example.backend.domain.cart.repository;
 
 import com.example.backend.domain.cart.entity.Cart;
+import com.example.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     boolean existsByProductId_IdAndMemberId_Id(Long productId, Long memberId);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.product WHERE c.member = :member")
+    List<Cart> findAllByMemberWithProducts(@Param("member") Member member);
 }
