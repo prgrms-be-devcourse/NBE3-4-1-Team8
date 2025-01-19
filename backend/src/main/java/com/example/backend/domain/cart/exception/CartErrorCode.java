ff3166377f6f756c54ce64ef02f03886d0c38276
wjwj910 <wjwj910@gmail.com>
2025-01-19 23:40:05 +0900

    feat: 장바구니 예외 코드 추가

--- backend/src/main/java/com/example/backend/domain/cart/exception/CartErrorCode.java
+++ backend/src/main/java/com/example/backend/domain/cart/exception/CartErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum CartErrorCode {
     ALREADY_EXISTS_IN_CART(HttpStatus.BAD_REQUEST, "이미 장바구니에 추가된 상품입니다.", "400-1"),
     INVALID_QUANTITY(HttpStatus.BAD_REQUEST,"상품을 최소 1개 이상 추가하여야 합니다." ,"400-2"),
-    INVALID_MEMBER(HttpStatus.FORBIDDEN,"요청한 회원 ID와 로그인한 회원 ID가 다릅니다." ,"403-1");
+    SAME_QUANTITY_IN_CART(HttpStatus.BAD_REQUEST, "현재 수량과 동일한 수량입니다.", "400-3"),
+    PRODUCT_NOT_FOUND_IN_CART(HttpStatus.NOT_FOUND, "장바구니에 존재하지 않는 상품입니다.", "404-1");
 
     final HttpStatus httpStatus;
     final String message;
