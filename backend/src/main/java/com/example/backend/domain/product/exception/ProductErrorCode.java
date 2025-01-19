bdad3a8f36c2436f7f5e9d23cc3bc3edde014f04
100gammmit <100minha@naver.com>
2025-01-19 14:13:41 +0900

    feat: 중복 상품명 검증 예외 추가
    EXISTS_NAME(HttpStatus.BAD_REQUEST, "400-2", "중복된 상품 이름입니다.");

--- backend/src/main/java/com/example/backend/domain/product/exception/ProductErrorCode.java
+++ backend/src/main/java/com/example/backend/domain/product/exception/ProductErrorCode.java
@@ -13,7 +13,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ProductErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다.", "404");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다.", "404"),
+    EXISTS_NAME(HttpStatus.BAD_REQUEST, "400-2", "중복된 상품 이름입니다.");
 
     final HttpStatus httpStatus;
     final String message;
