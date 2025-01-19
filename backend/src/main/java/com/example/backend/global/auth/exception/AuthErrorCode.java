e1ceea3d777ec4e0c9beb71937bd91504feec2f5
Kim Dong O <kdo_1999@naver.com>
2025-01-19 17:23:20 +0900

    refactor: 서버 오류랑 이메일 인증 관련 예외 코드 추가 #54

--- backend/src/main/java/com/example/backend/global/auth/exception/AuthErrorCode.java
+++ backend/src/main/java/com/example/backend/global/auth/exception/AuthErrorCode.java
@@ -12,6 +12,7 @@ import lombok.Getter;
  */
 @Getter
 public enum AuthErrorCode {
+	UNKNOWN_SERVER(HttpStatus.INTERNAL_SERVER_ERROR, "500-1", "요청 처리중 서버에서 예외가 발생했습니다."),
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "404-1", "해당 유저가 존재하지 않습니다."),
 	CERTIFICATION_CODE_NOT_FOUND(HttpStatus.NOT_FOUND, "404-2", "해당 이메일의 인증 코드 정보가 존재하지 않습니다."),
 
@@ -25,7 +26,8 @@ public enum AuthErrorCode {
 	REFRESH_TOKEN_NOT_MATCH(HttpStatus.UNAUTHORIZED, "401-8", "저장된 리프레시 토큰과 일치하지 않습니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "401-9", "리프레시 토큰을 찾을 수 없습니다."),
 
-	ALREADY_CERTIFIED(HttpStatus.BAD_REQUEST, "400-1", "이미 이메일 인증을 하셨습니다.");
+	ALREADY_CERTIFIED(HttpStatus.BAD_REQUEST, "400-1", "이미 이메일 인증을 하셨습니다."),
+	TOOMANY_RESEND_ATTEMPTS(HttpStatus.BAD_REQUEST, "400-2", "5회 이상 시도하셨습니다. 잠시후 다시 시도해주세요.");
 
 	final HttpStatus httpStatus;
 	final String code;
